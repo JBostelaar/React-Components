ee0a05db4b5cdbcba26a960365a78c869332bc60
Jorn Bostelaar <bostelaarj@gmail.com>
2016-08-03 22:39:01 +0200

    gulp: refactored styles task

--- gulp/index.js
+++ gulp/index.js
@@ -1,15 +1,13 @@
-'use strict';
-
 const globby = require('globby');
-const gulp   = require('gulp');
-const gutil  = require('gulp-util');
+const gulp = require('gulp');
+const gutil = require('gulp-util');
 
-globby.sync(__dirname + '/tasks/*.js').forEach(task => require(task));
+globby.sync(`${__dirname}/tasks/*.js`).forEach(task => require(task));
 
 const tasks = ['babel', 'scripts', 'styles', 'symlink'];
 
-if(gutil.env.dev){
-	tasks.push('nodemon', 'watch');
+if (gutil.env.dev) {
+	tasks.push('nodemon');
 }
 
-gulp.task('default', tasks)
+gulp.task('default', tasks);
