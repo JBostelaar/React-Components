d497177049ad96528560e314b901b12a27260e57
Jorn Bostelaar <bostelaarj@gmail.com>
2016-08-14 14:49:56 +0200

    Core: added component transtion animation

--- src/app/components/App.jsx
+++ src/app/components/App.jsx
@@ -1,19 +1,28 @@
 import React from 'react';
 import AppNav from 'app/components/AppNav';
+import TransitionGroup from 'react-addons-css-transition-group';
 
-export default class App extends React.Component {
-	render() {
-		return (
-			<main className="app">
-				<AppNav />
-				<div className="app__components">
-					{this.props.children}
-				</div>
-			</main>
-		);
-	}
+export default function App({ children, location }) {
+	return (
+		<main className="app">
+			<AppNav />
+			<div className="app__components">
+				<TransitionGroup
+					component="div" className="animation-container"
+					transitionName="componentsTransition"
+					transitionAppear transitionAppearTimeout={300}
+					transitionEnterTimeout={300} transitionLeaveTimeout={300}
+				>
+					{React.cloneElement(children, {
+						key: location.pathname,
+					})}
+				</TransitionGroup>
+			</div>
+		</main>
+	);
 }
 
 App.propTypes = {
 	children: React.PropTypes.object,
+	location: React.PropTypes.object,
 };
